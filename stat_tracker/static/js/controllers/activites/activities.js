853edfb5a85d15a57a4461f89bdf1dc9083bcfd0
lee allen <leeallen337@gmail.com>
2015-06-30 14:48:17 -0400

    successfully posting in ajax

--- stat_tracker/static/js/controllers/activites/activities.js
+++ stat_tracker/static/js/controllers/activites/activities.js
@@ -10,6 +10,7 @@ app.router.route('activities', function () {
     contentType: 'application/json',
   }).done(function (data) {
     $('.list-activites').text(data[0].title);
+    console.log(data);
   }).fail(function () {
     console.log(arguments);
   });
@@ -17,12 +18,40 @@ app.router.route('activities', function () {
   $('.main-content').on('click', '.activity-form button', function (e) {
     e.preventDefault();
     
+    // using jQuery
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken'); 
+    
+    function csrfSafeMethod(method) {
+      // these HTTP methods do not require CSRF protection
+      return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    
     var activity = {
       title: $('input[name="activity"]').val(),
       description: $('textarea[name="description"]').val()
     };
     
     $.ajax({
+      beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+      },
       url: '/api/activities/',
       method: 'POST',
       data: JSON.stringify(activity),
