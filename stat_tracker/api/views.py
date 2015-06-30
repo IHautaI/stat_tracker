85899fdf334cb6fa244a2331e2c3b79ebbdc3a42
Jeremy Gresham <greshjs@gmail.com>
2015-06-30 17:51:13 -0400

    cleaned up

--- stat_tracker/api/views.py
+++ stat_tracker/api/views.py
@@ -30,22 +30,6 @@ class ActivityViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(profile=self.request.user.profile)
 
-    # def get_serializer(self, *args, **kwargs):
-    #     if self.request.method in ['GET', 'POST', 'DELETE']:
-    #         return super().get_serializer(*args, **kwargs)
-    #
-    #     if self.request.method in ['PUT']:
-    #         serializer_class = EditActivitySerializer
-    #         kwargs['context'] = self.get_serializer_context()
-    #         return serializer_class(*args, **kwargs)
-
-    # def perform_update(self, serializer):
-    #     stats = self.request.data['stats']
-    #     activity = serializer.save()
-    #
-    #     serializer = EntrySerializer(stats)
-    #     activity.entry_set.create()
-
 
 @api_view(['PUT', 'POST'])
 @login_required
